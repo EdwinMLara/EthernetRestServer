f9e8a07bf8f268dccabef7c1cb1ad12748af1fe6
EdwinMLara <emlara35@gmail.com>
2021-01-08 15:16:24 -0600

    tratando de hacer funcionar el temporizador

--- ethernetServer.ino
+++ ethernetServer.ino
@@ -1,22 +1,32 @@
 #include <ArduinoJson.h>
-#include <Ethernet.h>  
 #include <SPI.h>
+#include <Ethernet.h>  
 #include <string.h>
 
 EthernetServer server(80);
 
 uint8_t encender = 2;
 uint8_t apagar = 7;
+
 bool status_sistem = false;
 bool banderaPulse = false;
+bool banderaTempo = false;
+
 boolean banderasTemporizador[4];
 
 unsigned long startmillis;
 unsigned long startmillisTemporizador;
 
+unsigned long teG1;
+unsigned long taG1;
+unsigned long teG2;
+unsigned long taG2;
+
 unsigned long desFase = 0;
+
 DynamicJsonDocument doc(200);
 DynamicJsonDocument doc2(200);
+
 void ethernetConfiguration(){
   byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
   byte ip[] = {10, 0, 0, 208};
@@ -31,31 +41,11 @@ void ethernetConfiguration(){
       delay(1);
     }
   }
-
   
   if (Ethernet.linkStatus() == LinkOFF) {
     Serial.println("El cable Ethernet no esta conectado.");
   }
-}
 
-void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-
-  iniciar_banderas(banderasTemporizador,4);
-  
-  Serial.println("Servidor Ethernet.");
-  
-  pinMode(encender,OUTPUT);
-  digitalWrite(encender,LOW);
-  pinMode(apagar,OUTPUT);
-  digitalWrite(apagar,LOW);
-
-  pinMode(4,OUTPUT);
-  digitalWrite(4,HIGH);
-
-  ethernetConfiguration();
-  
   server.begin();
   Serial.print("Server inicio en: ");
   Serial.println(Ethernet.localIP());
@@ -68,21 +58,59 @@ void iniciar_banderas(boolean *banderas, uint8_t tam){
   }
 }
 
-int* getBodyIndexPositions(char request[]){
-  static int positions[2] = {0,0};
-  int i=0;
-  while(request[i] != '\0'){
-      switch(request[i]){
-        case '{':
-          positions[0] = i;
-          break;
-        case '}':
-          positions[1] = i;
-          break;
-     }
-    i++;
+void configuracionTemporizador(uint8_t te1,uint8_t ta1,uint8_t te2,uint8_t ta2){
+  iniciar_banderas(banderasTemporizador,4);
+  teG1 = te1*60*1000;
+  taG1 = teG1 + ta1*60*1000;
+  teG2 = teG1 + taG1 + te2*60*1000;
+  taG2 = teG1 + taG1 + teG2 + ta2*60*1000;
+}
+
+char* Seguntos2Time(unsigned long tiempo){
+    char str[20] = "";
+    char aux[20] = "";
+    unsigned long reciduo,hr,minu,seg;
+    seg = tiempo % 60;
+    reciduo = tiempo / 60;
+    minu = reciduo % 60;
+    reciduo = reciduo / 60;
+    hr = reciduo%60;
+    itoa(hr,aux,10);
+    strcat(str,aux);
+    strcat(str," : ");
+    itoa(minu,aux,10);
+    strcat(str,aux);
+    strcat(str," : ");
+    itoa(seg,aux,10);
+    strcat(str,aux);
+    Serial.println("Adentro ");
+    Serial.println(str);
+
+    char *strReturn = malloc(20);
+    strcpy(strReturn, str);
+    return strReturn;
+}
+
+void temporizador(unsigned long countTime){
+  if((countTime >= teG1) && banderasTemporizador[0]){
+    Serial.println("Entro en 1");
+    banderasTemporizador[0] = false;
   }
-  return positions;
+  if((countTime >= taG1) && banderasTemporizador[1]){
+    Serial.println("Entro en 2");
+    banderasTemporizador[1] = false;
+  }
+  if((countTime >= teG2) && banderasTemporizador[2]){
+    Serial.println("Entro en 3");
+    banderasTemporizador[2] = false;
+  }
+  if((countTime >= taG2) && banderasTemporizador[3]){
+    banderasTemporizador[3] = false;
+    iniciar_banderas(banderasTemporizador,4);
+    desFase = 0;
+    Serial.println("Reinicio"); 
+  }
+
 }
 
 void lunchPulse(uint8_t rele){
@@ -103,26 +131,29 @@ void pulseControl(unsigned long countTime,uint8_t seconds){
     }
 }
 
-void temporizador(unsigned long countTime,uint8_t te,uint8_t ta,uint8_t te2,uint8_t ta2){
-   if((countTime >= te) && banderasTemporizador[0]){
-    Serial.println("Entro en 1");
-    banderasTemporizador[0] = false;
-  }
-  if((countTime >= (te + ta)) && banderasTemporizador[1]){
-    Serial.println("Entro en 2");
-    banderasTemporizador[1] = false;
-  }
-  if((countTime >= (te + ta + te2)) && banderasTemporizador[2]){
-    Serial.println("Entro en 3");
-    banderasTemporizador[2] = false;
-  }
-  if((countTime >= (te + ta + te2 + ta2)) && banderasTemporizador[3]){
-    banderasTemporizador[3] = false;
-    iniciar_banderas(banderasTemporizador,4);
-    desFase = 0;
-    Serial.println("Reinicio"); 
-  }
+void desFaseTime(uint8_t desfase){
+  unsigned long *aux = &desFase;
+  Serial.print(*aux);
+  Serial.print(" ");
+  *aux += desfase*60*1000;
+  Serial.println(desFase);
+}
 
+int* getBodyIndexPositions(char request[]){
+  static int positions[2] = {0,0};
+  int i=0;
+  while(request[i] != '\0'){
+      switch(request[i]){
+        case '{':
+          positions[0] = i;
+          break;
+        case '}':
+          positions[1] = i;
+          break;
+     }
+    i++;
+  }
+  return positions;
 }
 
 uint8_t getTypeRequest(const char request[]){
@@ -140,6 +171,9 @@ uint8_t getTypeRequest(const char request[]){
       int bodyLength = (indexs[1] - indexs[0]) + 3;
       char body[bodyLength];
       memcpy(body,&request[indexs[0]],bodyLength);
+
+      Serial.println(body);
+      delay(20);
       DeserializationError error = deserializeJson(doc2,body);
       
       if (error) {
@@ -148,25 +182,31 @@ uint8_t getTypeRequest(const char request[]){
         return 0;
       }
 
-      uint8_t encender_val = doc2["encender"];
-      uint8_t apagar_val = doc2["apagar"];
-      uint8_t tempo = doc2["tempo"];
-      uint8_t desfase = doc2["desfase"];
+      delay(10);
 
-      Serial.print(encender_val);
-      Serial.print(" ");
-      Serial.print(apagar_val);
-      Serial.print(" ");
-      Serial.print(tempo);
-      Serial.print(" ");
-      Serial.println(desfase);
+      if(doc.containsKey("tempoConfig")){
+        Serial.println("Configurando Temporizador");
+      }else{
+        uint8_t encender_val = doc2["encender"];
+        uint8_t apagar_val = doc2["apagar"];
+        uint8_t tempo = doc2["tempo"];
+        uint8_t desfase = doc2["desfase"];
 
-      if(encender_val)
-        lunchPulse(encender);
-
-      if(apagar_val)
-        lunchPulse(apagar);
-
+        if(encender_val)
+          lunchPulse(encender);
+  
+        if(apagar_val)
+          lunchPulse(apagar);
+  
+        if(tempo){
+          Serial.println("Se encendio el Temporizador");
+        }
+        
+        if(desfase){
+          Serial.println("defasamiento en la señal");  
+        }
+      }
+       
       doc2.clear();
       typeRequest = 1; 
   }else if(strcmp(auxRequest,"GET") == 0){
@@ -176,18 +216,46 @@ uint8_t getTypeRequest(const char request[]){
   return typeRequest;
 }
 
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  startmillis = 0;
+  startmillisTemporizador = 0;
+  configuracionTemporizador(4,2,5,13);
+
+  Serial.println("Configuracion Temporizador: ");
+  Serial.println(teG1);
+  Serial.println(taG1);
+  Serial.println(teG2);
+  Serial.println(taG2);
+  
+  Serial.println("Servidor Ethernet.");
+  
+  pinMode(encender,OUTPUT);
+  digitalWrite(encender,LOW);
+  
+  pinMode(apagar,OUTPUT);
+  digitalWrite(apagar,LOW);
+
+  pinMode(4,OUTPUT);
+  digitalWrite(4,HIGH);
+
+  ethernetConfiguration();
+}
+
 void loop() {
    EthernetClient client = server.available();
    if(client){
     String header;
     bool bandera = true;
+    char *aux,aux2;
     Serial.println("Nuevo cliente conectado");
-    delay(5);
       while(client.connected() && bandera){
           if(client.available()){
               
               uint8_t typeRequest = 0,count = 0;
-              char request[500],*aux;
+              char request[500];
               
               while(client.available() && count < 500){
                 char c;
@@ -202,39 +270,61 @@ void loop() {
               
               switch(typeRequest){
                 case 1: 
-                  doc["status"] = 200;
-                  doc["result"] = "request succes";
+                  doc["status_request"].set(200);
+                  doc["result"].set("request success");
                   break;
                 case 2:
-                  doc["status_sistem"] = status_sistem ;
+                  doc["status_sistem"].set(status_sistem);
+                  doc["status_Tempo"].set(banderaTempo);
+                  doc["desfase"].set(desFase);
+                  /*if(banderaTempo){
+                    unsigned long currentmillis,countTime2;
+                    currentmillis = millis(); 
+                    countTime2 = (currentmillis - startmillisTemporizador) + desFase;
+                    aux2 = Seguntos2Time(countTime2/1000); 
+                    Serial.println(aux2);
+                  }*/
                   break;
                 default:
-                  doc["status"] = 200;
-                  doc["result"] = "error request";
+                  doc["status"].set(200);
+                  doc["result"].set("error request");
               }
-
+              
+              char output[80]="";
+              serializeJson(doc,output);
+              Serial.println(output);
+              
+              delay(10);
               client.println("HTTP/1.1 200 OK"); 
-              client.println("Access-Control-Allow-Origin:*");
               client.print("Content-Length: ");
               client.println(measureJson(doc));
               client.println("Content-Type: application/json");
               client.println();
-              serializeJson(doc, client);
+              
+              client.println(output);
               break;   
           }   
       }
-
-      doc.clear();
+      delay(10);
       client.stop();
       client.flush();
-      delay(5);
+      doc.clear();
       Serial.println("");
       Serial.println("Cliente desconectado");
   }
+  
 
   if(banderaPulse){
     unsigned long countTime; 
-    countTime = (millis() - startmillis);
+    countTime = millis() - startmillis;
     pulseControl(countTime,5);
   }
+
+  if(banderaTempo){
+    unsigned long countTime; 
+    countTime = (millis() - startmillisTemporizador) + desFase;
+    temporizador(countTime);
+  }
+
+  delay(10);
 }
